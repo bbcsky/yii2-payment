eec391c5b40a820990b268937bc05f966f96c578
zhouxiaowen <zhouxiaowen@bbfstore.com>
2015-12-22 19:36:37 +0800

    add alipay

--- Alipay.php
+++ Alipay.php
@@ -4,31 +4,47 @@ namespace bbcsky\payment;
 use Yii;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
+use yii\base\InvalidValueException;
 
 class Alipay extends Component
 {
     public $notify_url = '';
-    public $sign_type = 'RSA';
+    //public $sign_type = 'RSA';
     public $input_charset = 'utf-8';
-    public $transport = 'http';
+    //public $transport = 'http';
     public $service = 'mobile.securitypay.pay';
     public $service_refund = 'refund_fastpay_by_platform_pwd';
     public $gateway_refund = 'https://mapi.alipay.com/gateway.do';
     public $order_pre = 'Alipay';
 
     private $partner;
-    private $seller_id;
     private $key;
     private $key_path;
     private $ali_pub_path;
-    private $ali_ca_path;
-    private $cacert;
-    private $curl_proxy_host;
-    private $curl_proxy_port;
+    private $notify_data;
+    //private $ali_ca_path;
+    //private $cacert;
+    //private $curl_proxy_host;
+    //private $curl_proxy_port;
 
-    private function checkParams()
+    public function setPartner($partner)
     {
+        $this->partner = $partner;
+    }
 
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    public function setKey_path($key_path)
+    {
+        $this->key_path = Yii::getAlias($key_path);
+    }
+
+    public function setAli_pub_path($ali_pub_path)
+    {
+        $this->ali_pub_path = Yii::getAlias($ali_pub_path);
     }
 
     public function init()
@@ -39,12 +55,8 @@ class Alipay extends Component
         {
             if(empty($this->{$need}))
             {
-                throw new InvalidConfigException(get_class($this) . " must define alipay's {$need}.");
+                throw new InvalidConfigException(get_class($this) . " must define alipay's params {$need}.");
             }
-        }
-        if(empty($this->seller_id))
-        {
-            $this->seller_id = $this->partner;
         }
     }
 
@@ -65,17 +77,24 @@ class Alipay extends Component
      */
     public function prepay($order)
     {
+        $needs = array('title','order_sn','body','total_fee');
+        foreach($needs as $need)
+        {
+            if(!isset($order[$need]))
+            {
+                throw new InvalidConfigException(get_class($this) . " \$order 中必须包含键 {$need}.");
+            }
+        }
         $paras = ['subject' => $order['title']];
         $paras['out_trade_no'] = $this->order_pre.$order['order_sn'];
         $paras['body'] = $order['body'];
         $paras['total_fee'] = round($order['total_fee'],2);
         if($paras['total_fee'] <= 0)
         {
-            return '支付金额必须大于0';
+            throw new InvalidValueException(get_class($this) . " 支付金额必须大于0");
         }
         $paras['notify_url'] = $this->notify_url;
         return $paras;
-
     }
 
     /**
@@ -85,71 +104,67 @@ class Alipay extends Component
      */
     public function refund($order)
     {
+        $time = explode(' ', microtime());
+        $batch_no = date('Ymd', $time[1]) . substr($time[1], -5) . substr($time[0], 2, 6) . rand(10000, 99999);
+        $batch_num = 0;
+        $detail_data = '';
+        if(!empty($order['serial_no']) && !empty($order['pay_amount']))
+        {
+            $batch_num = 1;
+            $detail_data = $order['serial_no'] . '^' . $order['pay_amount'] . '^';
+            if(!empty($order['remark']) && strlen($order['remark']) <= 100)
+            {
+                $detail_data .= $order['remark'];
+            }
+        }
+        else
+        {
+            foreach($order as $o)
+            {
+                if(isset($o['serial_no']) && isset($o['pay_amount']))
+                {
+                    $batch_num ++;
+                    $detail_data = $o['serial_no'] . '^' . $o['pay_amount'] . '^';
+                    if(!empty($order['remark']) && strlen($order['remark']) <= 100)
+                    {
+                        $detail_data .= $order['remark'];
+                    }
+                    $detail_data .= '#';
+                }
+            }
+            if(empty($detail_data))
+            {
+                throw new InvalidValueException(get_class($this) . " \$order 中必须包含键 serial_no,pay_amount或属于他们的数组.");
+            }
+            $detail_data = substr($detail_data,0,-1);
+        }
         $paras = [
             'service'           =>$this->service_refund,
             'partner'           =>$this->partner,
             '_input_charset'    =>$this->input_charset,
-            //'sign_type'         =>$this->sign_type,
             'sign_type'         =>'MD5',
             'notify_url'        =>$this->notify_url,
             //'seller_email'      =>'',
             'seller_user_id'    =>$this->partner,
             'refund_date'       =>date('Y-m-d H:i:s'),
-            'batch_no'          =>$order['batch_no'],
-            'batch_num'         =>$order['batch_num'],
-            'detail_data'       =>$order['detail_data'],
+            'batch_no'          =>$batch_no,
+            'batch_num'         =>$batch_num,
+            'detail_data'       =>$detail_data,
         ];
-
-        ksort($paras);
-        reset($paras);
-        $app_str = '';
-        foreach($paras as $key => $val)
-        {
-            if($key == "sign" || $key == "sign_type" || $val == "")
-            {
-                if($val == "")
-                {
-                    unset($paras[$key]);
-                }
-                continue;
-            }
-            $app_str .= $key.'='.$val.'&';
-        }
-        if(get_magic_quotes_gpc()){
-            $app_str = stripslashes($app_str);
-        }
-        $paras['sign'] = $this->md5Sign(substr($app_str,0,-1));
+        $paras['sign'] = $this->md5Sign($this->makeStr($paras));
         return $this->buildRequestForm($paras, 'POST', 'submit');
     }
 
     public function notify()
     {
         $paras = $_POST;
-        ksort($paras);
-        reset($paras);
-        $sign_str = '';
-        foreach($paras as $key => $val)
+        if(strtoupper($paras['sign_type']) == 'MD5')
         {
-            if($key == "sign" || $key == "sign_type" || $val == "")
-            {
-                if($val == "")
-                {
-                    unset($paras[$key]);
-                }
-                continue;
-            }
-            $sign_str .= $key.'='.$val.'&';
-        }
-        if(strtolower($paras['sign_type']) == 'md5')
-        {
-            if(get_magic_quotes_gpc()){
-                $sign_str = stripslashes($sign_str);
-            }
-            $sign = $this->md5Verify(substr($sign_str,0,-1),$paras['sign']);
+            $sign = $this->md5Verify($this->makeStr($paras),$paras['sign']);
         }
         else
         {
-            $sign = $this->rsaVerify(substr($sign_str,0,-1),$paras['sign']);
+            $sign = $this->rsaVerify($this->makeStr($paras),$paras['sign']);
         }
         if($sign)
         {
@@ -166,11 +181,12 @@ class Alipay extends Component
             {
                 $paras['total_fee'] = 0;
             }
+            $this->notify_data = $paras;
             return $paras;
         }
         else
         {
-            return '签名失败';
+            throw new InvalidValueException(get_class($this) . " 验证签名失败.");
         }
     }
 
@@ -198,7 +214,10 @@ class Alipay extends Component
      */
     public function getTotalFee()
     {
-
+        if(isset($this->notify_data['total_fee']))
+        {
+            return $this->notify_data['total_fee'];
+        }
     }
 
     /**
@@ -207,7 +226,10 @@ class Alipay extends Component
      */
     public function getSerialNo()
     {
-
+        if(isset($this->notify_data['trade_no']))
+        {
+            return $this->notify_data['trade_no'];
+        }
     }
 
     /**
@@ -256,14 +278,40 @@ class Alipay extends Component
         return $result;
     }
 
+    private function makeStr($paras)
+    {
+        ksort($paras);
+        reset($paras);
+        $app_str = '';
+        foreach($paras as $key => $val)
+        {
+            if($key == "sign" || $key == "sign_type" || $val == "")
+            {
+                if($val == "")
+                {
+                    unset($paras[$key]);
+                }
+                continue;
+            }
+            $app_str .= $key.'='.$val.'&';
+        }
+        return substr($app_str,0,-1);
+    }
+
     private function md5Sign($prestr)
     {
+        if(get_magic_quotes_gpc()){
+            $prestr = stripslashes($prestr);
+        }
         $prestr = $prestr . $this->key;
         return md5($prestr);
     }
 
     private function md5Verify($prestr, $sign)
     {
+        if(get_magic_quotes_gpc()){
+            $prestr = stripslashes($prestr);
+        }
         $prestr = $prestr . $this->key;
         $mysgin = md5($prestr);
         if($mysgin == $sign) {
@@ -273,6 +321,7 @@ class Alipay extends Component
             return false;
         }
     }
+
     private function buildRequestForm($para, $method, $button_name)
     {
         $sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='".$this->gateway_refund."' method='".$method."'>";
